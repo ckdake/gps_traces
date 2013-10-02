dd9611b0d800e9a4dc519d0aacab48c46c712122
Chris Kelly <ckdake@ckdake.com>
2013-10-02 00:32:02 -0400

    Splits by year

--- mykml2csv.rb
+++ mykml2csv.rb
@@ -4,6 +4,7 @@ require 'json'
 doc = Nokogiri::XML(File.open('/Users/ckdake/Library/Application Support/Google Earth/myplaces.kml'))
 
 combined = { type: 'MultiLineString', coordinates: [] }
+years = {}
 
 doc.css('Placemark').each do |p|
   name = p.css('name').text
@@ -13,9 +14,21 @@ doc.css('Placemark').each do |p|
                .gsub(/\s+/, '_')
     coordinates = p.css('coordinates').first.text.split(' ').collect{|i| i.split(',')}
     combined[:coordinates] += coordinates
+
+    year = name[0,4]
+    years[year] ||= []
+    years[year] += coordinates
+
     open("split/#{name}.geojson", 'w') { |f|
       f.puts({ type: 'LineString', coordinates: coordinates }.to_json)
     }
   end
 end
+
 open("combined.geojson", 'w') { |f| f.puts combined.to_json }
+
+years.each_pair do |year,coordinates|
+  open("years/#{year}.geojson", 'w') { |f|
+    f.puts({ type: 'MultiLineString', coordinates: coordinates }.to_json)
+  }
+end
